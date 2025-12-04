d3ee2d2c6379047397b6c28a18a3c056aebd1a13
louzhu <971393182@qq.com>
2025-12-04 15:49:09 +0800

    router修改

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import Layout from '../pages/Layout/index.jsx'
 import Login from '../pages/Login/index.jsx'
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
 import { Navigate } from 'react-router-dom'
 import Home from '@/pages/Layout/Home/home.jsx'
 import Artical from '@/pages/Layout/Artical/artical.jsx'
 import Publish from '@/pages/Layout/Publish/publish.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <AuthRoute><Layout /></AuthRoute>,
